8d32dd5edfa01a5dfbf35078f5e3e1c7788950c1
WDSLab <odslab.official@gmail.com>
2025-08-11 22:18:11 +0900

    update

---
+++ Train.py
@@ -0,0 +1,62 @@
+import Preprocessing
+import model.CRU as cru
+
+from sklearn.metrics import mean_squared_error as mse
+import numpy as np
+import matplotlib.pyplot as plt
+
+import torch 
+import torch.nn as nn
+import torch.optim as optim
+
+class ModelTrain():
+    
+    def __init__(self, data_name, data_type, time_window, forecasting_term):
+
+        self.in_dim = time_window
+        self.data_name = data_name
+        pre = Preprocessing._Preprocessing(data_name, data_type, time_window, forecasting_term)
+        self.trainX, self.trainY, self.testX, self.testY, self.scaler = pre.preprocessing()
+
+        if torch.cuda.is_available():
+            self.device = torch.device('cuda')
+        else:
+            self.device = torch.device('cpu')
+                    
+    def _fit(self, hid_dim, out_dim, num_layers, epoch):
+        
+        m = cru._CRU(self.in_dim, hid_dim, out_dim, num_layers).to(self.device)
+        crit = nn.MSELoss()
+        para = list(m.parameters())
+        optimizer = optim.Adam(para, 0.0001)
+        
+        for e in range(epoch):
+            optimizer.zero_grad()
+            out, _ = m(self.trainX)
+            loss = crit(out, self.trainY.view(-1,1))
+            loss.backward()
+            optimizer.step()
+        
+            if (e+1) % 100 == 0:
+                with torch.no_grad():
+                    pred,_ = m(self.testX)
+                    pred = pred.cpu().detach().numpy()
+                    real = self.testY.cpu().numpy().reshape(-1,1)
+                    for i in range(len(self.scaler)):
+                        pred[i,:] = self.scaler[i].inverse_transform(pred[i,:].reshape(-1,1))
+                        real[i,:] = self.scaler[i].inverse_transform(real[i,:].reshape(-1,1))
+                        
+                    test_mape = np.mean(abs((np.array(pred)-np.array(real))/np.array(pred)))*100
+                    test_rmse = mse(pred, real)**0.5
+                
+                print('[Epoch: {}/{}] [Train Loss: {}] [Test RMSE: {}] [Test MAPE: {}]'.format(
+                    e+1, epoch, str(loss.item())[:6], str(test_rmse)[:6], str(test_mape)[:6]))
+
+
+        return m
+        
+                        
+                        
+                        
+                        
+                    
---
+++ model/CRUCell.py
@@ -0,0 +1,102 @@
+import torch 
+import torch.nn as nn
+from torch.autograd import Variable
+from statsmodels.tsa.seasonal import STL
+
+import pandas as pd
+import numpy as np
+
+
+class _CRUCell(nn.Module):
+    
+    def __init__(self, in_dim, hid_dim, bias=True):
+        super(_CRUCell, self).__init__()
+        
+        self.in_dim = in_dim
+        self.hid_dim = hid_dim
+        self.bias = bias
+        
+        self.wx_t = nn.Linear(self.in_dim, self.hid_dim*3, bias = self.bias)
+        self.wx_s = nn.Linear(self.in_dim, self.hid_dim*3, bias = self.bias)
+        self.wx_r = nn.Linear(self.in_dim, self.hid_dim, bias = self.bias)
+        
+        
+        self.wh_t = nn.Linear(self.hid_dim, self.hid_dim*3, bias = self.bias)
+        self.wh_s = nn.Linear(self.hid_dim, self.hid_dim*3, bias = self.bias)
+        self.wh_r = nn.Linear(self.hid_dim, self.hid_dim, bias = self.bias)
+        
+        self.reset_parameters()
+
+        if torch.cuda.is_available():
+            self.device = torch.device('cuda')
+        else:
+            self.device = torch.device('cpu')
+            
+            
+    def forward(self, x, hid_state = None):
+        
+        lamda = 0.5
+        x_t, x_s, x_r = self.ts_decompose(x)
+        
+        if hid_state is None:
+            hid_state = Variable(3, x.size(1), self.hid_dim).to(self.device)
+        else:
+            hid_state.cuda()
+        
+        with torch.no_grad():
+            print(x_t.size())
+            x_t = self.wx_t(x_t).to(self.device)
+            x_s = self.wx_s(x_s).to(self.device)
+            x_r = self.wx_r(x_r).to(self.device)
+            
+            h_t = self.wh_t(hid_state[0,:,:]).to(self.device)
+            h_s = self.wh_s(hid_state[1,:,:]).to(self.device)
+            
+        x_autocor_t, x_cor_t, x_new_t = (x_t).chunk(3,1)
+        x_autocor_s, x_cor_s, x_new_s = (x_s).chunk(3,1)
+        h_autocor_t, h_cor_t, h_new_t = (h_t).chunk(3,1)
+        h_autocor_s, h_cor_s, h_new_s = (h_s).chunk(3,1)
+        
+        autocor_t = torch.sigmoid(x_autocor_t + h_autocor_t)
+        autocor_s = torch.sigmoid(x_autocor_s + h_autocor_s)
+        
+        cor_t = torch.sigmoid(x_cor_t + h_cor_s)
+        cor_s = torch.sigmoid(x_cor_s + h_cor_t)
+        
+        new_t = lamda*torch.tanh(x_new_t + (autocor_t * h_new_t)) + (1-lamda)*torch.tanh(x_new_t + (cor_t * h_new_s))
+        new_s = lamda*torch.tanh(x_new_s + (autocor_s * h_new_s)) + (1-lamda)*torch.tanh(x_new_s + (cor_s * h_new_t))
+        
+       
+        with torch.no_grad():
+            hid_state[0,:,:] = new_t
+            hid_state[1,:,:] = new_s
+            hid_state[2,:,:] = torch.tanh(x_r)
+        
+        return hid_state
+
+    def reset_parameters(self):
+        std = 1.0 / np.sqrt(self.hid_dim)
+        for w in self.parameters():
+            w.data.uniform_(-std, std)
+
+    def ts_decompose(self, x):
+        
+        x = pd.DataFrame(x.cpu().numpy())
+        
+        dates = pd.date_range('1990-01-01', periods = len(x), freq='D')
+        x.index = dates
+        trend = []
+        seasonal = []
+        resid = []
+        for i in range(len(x.columns)):
+            stl = STL(x[i])
+            res = stl.fit()
+            trend.append(res.trend.values)
+            seasonal.append(res.seasonal.values)
+            resid.append(res.resid.values)
+        
+        trend = torch.FloatTensor(trend).permute(1,0).to(self.device)
+        seasonal = torch.FloatTensor(seasonal).permute(1,0).to(self.device)
+        resid = torch.FloatTensor(resid).permute(1,0).to(self.device)
+        
+        return trend, seasonal, resid
